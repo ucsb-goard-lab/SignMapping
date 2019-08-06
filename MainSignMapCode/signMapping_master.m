1ca4d829743011958ad7c88e735ae670cb95197f
kevinksit <kevinkamansit@gmail.com>
2019-08-06 13:13:52 -0700

    Separated the autorun from the master script into its own function, autoGetSignMap

--- MainSignMapCode/signMapping_master.m
+++ MainSignMapCode/signMapping_master.m
@@ -1,35 +1,33 @@
-% signMapping_master
+% This script lets you autorun or just use each part individually
 
+% Written 06Aug2019 KS
+% Updated
+
+% Adding necessary paths to access the code for sign mapping
 curr_script_dir = mfilename('fullpath');
 parent_script_dir = curr_script_dir(1:end-17);
 addpath(fullfile(parent_script_dir,'Juavinett et al 2017 Code'));
 
-%% Usage
+sm = SignMapper(); % Create the sign mapping object
 
-sm = SignMapper();
+data_loc = sm.getUserInput(); % Get the input for everything
 
-%% If you just want a single thing to run, use this:
+[data, stimdata] = sm.getData(data_loc); % Get and process data into a usable state
 
-sm.autoRunMapping();
+[aziResp,altResp] = sm.separateResponseData(data,stimdata); % Separate each recording into the cardinal directions, based on timestamps
 
-%{
-%% Advanced: Individual running of each module for troubleshooting/more control
-data_loc = sm.getUserInput();
-
-[data, stimdata] = sm.getData(data_loc);
-
-[aziResp,altResp] = sm.separateResponseData(data,stimdata);
-
+% Run fourier transforms
 fourier_data(:,:,:,1) = fft(aziResp(:,:,:,1),[],3);
 fourier_data(:,:,:,2) = fft(aziResp(:,:,:,2),[],3);
 fourier_data(:,:,:,3) = fft(altResp(:,:,:,1),[],3);
 fourier_data(:,:,:,4) = fft(altResp(:,:,:,2),[],3);
 
-k = sm.findRetinotopicMap(fourier_data);
+k = sm.findRetinotopicMap(fourier_data); % Find the correct harmonic for retinotopic maps
 
-[azi,alt] = sm.getRetinotopicMap(fourier_data,k);
-sm.displayMaps(azi,alt);
+[azi,alt] = sm.getRetinotopicMap(fourier_data,k); % Get the retinotopic map of determined harmonic
+sm.displayMaps(azi,alt); % Show maps
 
+% Below allows you to manually redefine maps if the auto-chooser got the wrong harmonic
 while true
     goodmap = questdlg('Do your maps look good?','Map quality','Yes','No','Yes');
     close
@@ -45,10 +43,7 @@ while true
 end
 
 mkdir('AdditionalSignMapMaterials'); % Additional save directory for supplemental stuff
-maps = sm.Juavinett2017_signMapping(azi,alt);
+maps = sm.Juavinett2017_signMapping(azi,alt); % Run the sign map creator, from the phase-maps
 
-sm.saveSignMaps(maps);
-
-%}
-
+sm.saveSignMaps(maps); % Save everything
 
